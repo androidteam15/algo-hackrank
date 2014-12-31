3183664555698f4174bcca539b58e1e45da87721
jhlee <jjjeeettt15@gmail.com>
2014-12-31 13:41:46 +0900

    haha

---
+++ Snasa_and_Xor.cpp
@@ -0,0 +1,40 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <map>
+
+#define miterator map<int, int>::iterator
+#define MAXN 100000
+using namespace std;
+
+
+
+int main() {
+	int t,n;
+	int len;
+	A[MAXN];
+	cin >> t;
+	cin >> n;
+
+	for(int i=0; i<t; i++){
+		for(int j=0; j<n; j++) cin >> A[j];
+
+		len =0;
+		for(int j=0; j<n; j++){
+			
+
+		}
+
+
+	}
+    map<int,int> m;
+
+    for(int i=0; i<) 
+    cout << a << endl;
+
+
+    return 0;
+		}
+}
